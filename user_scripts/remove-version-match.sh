112a22ed8583647191e690aa5886f1e80a1df4ce
Xenhat Hex <commits@xenh.at>
2020-05-03 14:39:52 -0400

    moved remove-version-match script to gitlab.com/src_prepare/scripts

--- user_scripts/remove-version-match.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ -z $1 ]; then
-	echo "Must specify a file to modify..."
-	exit 1
-fi
-regex='^=?(\S+)(?:-[[:digit:]]+\S*)(\s)(\S+)$'
-sub='\1\2\3'
-perl -pe "s;$regex;$sub;gm" -i "${1}"
