gentoo-mirror/src_prepare-overlay

commit ca24629b4a9abb12171f639428226e83b18d6ffa

app-misc/spicetify-cli: add 2.36.12
Signed-off-by: Alfred Wingate <parona@protonmail.com>

app-misc/spicetify-cli/spicetify-cli-2.36.12.ebuild
@@ -1,0 +1,102 @@
+# Copyright 1999-2024 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit go-module
+
+MY_PN="cli"
+
+WHITELIST_VERSIONS=(
+	"<> 1.2.8 1.2.39"
+)
+
+whitelist_versions() {
+	local version_spec version1 version2
+	local -a deps
+
+	SPOTIFY_VERSIONS=
+
+	# Iterate in reverse for elog
+	for ((index = $(( ${#WHITELIST_VERSIONS[@]} - 1 )); index >= 0; index--)); do
+		read -r version_spec version1 version2 <<< "${WHITELIST_VERSIONS[${index}]}"
+
+		case ${version_spec} in
+			"<>")
+				deps[$index]="( <=media-sound/spotify-${version2} >=media-sound/spotify-${version1} )"
+				SPOTIFY_VERSIONS+="${version1} -> ${version2}"
+				;;
+			"~")
+				deps[$index]="~media-sound/spotify-${version1}"
+				SPOTIFY_VERSIONS+="${version1}"
+				;;
+			*)
+				die "Invalid version specifier in WHITELIST_VERSIONS"
+				;;
+		esac
+
+		if [[ ${index} == 0 ]]; then
+			SPOTIFY_VERSIONS+=". "
+		elif [[ ${index} == 1 ]]; then
+			SPOTIFY_VERSIONS+=" and "
+		else
+			SPOTIFY_VERSIONS+=", "
+		fi
+	done
+
+	RDEPEND=" || ( ${deps[@]} )"
+}
+whitelist_versions
+
+DESCRIPTION="Commandline tool to customize Spotify client."
+HOMEPAGE="https://spicetify.app/"
+SRC_URI="
+	https://github.com/spicetify/cli/archive/v${PV}.tar.gz -> ${P}.tar.gz
+	https://gitlab.com/api/v4/projects/37881342/packages/generic/${PN}/${PV}/${P}-deps.tar.xz
+"
+S="${WORKDIR}/${MY_PN}-${PV}"
+
+LICENSE="Apache-2.0 BSD LGPL-2.1 MIT"
+SLOT="0"
+KEYWORDS="~amd64"
+
+# no tests
+RESTRICT="test"
+
+INSTALLDIR="/opt/${PN}"
+
+src_compile() {
+	ego build -ldflags "-X main.version=${PV}"
+}
+
+src_install() {
+	insinto "${INSTALLDIR}"
+	doins -r {CustomApps,Extensions,Themes,jsHelper,spicetify-cli}
+	dobin "${FILESDIR}/spicetify"
+	fperms +x "${INSTALLDIR}/spicetify-cli"
+}
+
+pkg_postinst() {
+	elog "Spicetify requires a Spotify install that it can modify."
+	elog "To give read and write permissions to everyone on the system to run the following commands as root."
+	elog "# chmod a+wr /opt/spotify/spotify-client"
+	elog "# chmod a+wr /opt/spotify/spotify-client/Apps -R"
+	elog ""
+	elog "WARNING: Do not run spicetify as root please"
+	elog ""
+	elog "Spicetify compatibility is limited to the following Spotify versions:"
+	elog " ${SPOTIFY_VERSIONS}"
+	elog ""
+	elog "Otherwise you can install spotify to a user modifiable location like as a flatpak:"
+	elog " https://spicetify.app/docs/advanced-usage/installation#spotify-installed-from-flatpak"
+	elog ""
+	elog "To install themes see:"
+	elog " https://spicetify.app/docs/advanced-usage/themes"
+
+	if $(has_version -r ">=media-sound/spotify-1.2.25"); then
+		elog ""
+		elog "The New Releases custom app no longer works with Spotify 1.2.25 and higher. As an alternative,"
+		elog "please use What's New which was created by Spotify and can be enabled via Experimental Features"
+		elog "if it isn't already enabled."
+	fi
+}

app-misc/spicetify-cli/Manifest
@@ -2,3 +2,5 @@ DIST spicetify-cli-2.36.10-deps.tar.xz 5223544 BLAKE2B 8e3f700d6f0228936bce557e4
 DIST spicetify-cli-2.36.10.tar.gz 2018364 BLAKE2B 7e19088599bfef2acd990e3f34900f3c2cbc079dfeec984b171dad76af8352cae676860f80397ec8c940536fd00bbad241eb8502bf13062934922411eeb9b7c0 SHA512 1751dba60a9ca054bc0bc1dc712d2cef28342ebdce4cee1be3697c393915b4999c19eacbc4ab4c401e1056b91c37f4734dd59a1fd22d077e0d915e40b0846324
 DIST spicetify-cli-2.36.11-deps.tar.xz 5223520 BLAKE2B baca6229ff613537375da05f7d7c25e6c44301f3e2dd7b30d31f11bc77d5c89c9f8b5e8be5201127ad5d8a2f3356b3e701b837b555dfbe52145cfeb4ef7ea0e3 SHA512 167cc92e9f6fe249ac025185e587fd75d0a61b670b65d5496b19a1cc9826c9509858e6b947d7b06d1ed016bc1121ba0b1680e4cc8e6472624dff92dc35891a99
 DIST spicetify-cli-2.36.11.tar.gz 2018637 BLAKE2B 0822e88c915f33bf3cdfb743ff133e3fff94c7ab4c5cbfe5033492601998ad5edf0940e2ec18005d1ca6140616ef40d33e50281ff00ca8499db14c059614ffe5 SHA512 88e8b07be6207a3d2781dbd1a29b504ab424356c22171de4617e4c1fcf1d19c7f68b17289f85f8342fec026d87d86009bb9e8ef2323db6617d89e64ba0058a0b
+DIST spicetify-cli-2.36.12-deps.tar.xz 5298444 BLAKE2B 3c4e154e600c0c0eba2a89d74cd3542c24d7c6ccfcab08405514cb070451944144ab709d562881b35c806f27c1be41d5f2880ac57d89941d3a6e66ded2796fc6 SHA512 f14b1a95a037da63ab74c1073b5f7ab26924b2e77bf65a6ae3c436f349969d5e74f923b24ca9c35eba4c60c301e76aec8c7887da683ee08885317e867aa537d2
+DIST spicetify-cli-2.36.12.tar.gz 2019215 BLAKE2B f400f43999816e3c962526332da042c716fdc04d7f7dfe0522371d77cf74dc755e88497d7aeac8ce58005f60a5a303a4955e358d7c5f85b3febb1bf2fba3499b SHA512 53506979040469e5f0b03daddf25e3f3ba3ba1a2adad4d0367e8484d496753ceae470bb929f46c0b52c997cc7f7648c00ab9d23dd851e8da12d209cd938e1b0a

commit 72f1d283536616476fc578cd1343ed9650307acc

app-misc/spicetify-cli: add unstaged changes that were forgotten
Fixes: ca24629b4a9abb12171f639428226e83b18d6ffa
Signed-off-by: Alfred Wingate <parona@protonmail.com>

app-misc/spicetify-cli/spicetify-cli-2.36.12.ebuild
@@ -71,9 +71,12 @@ src_compile() {
 
 src_install() {
 	insinto "${INSTALLDIR}"
-	doins -r {CustomApps,Extensions,Themes,jsHelper,spicetify-cli}
-	dobin "${FILESDIR}/spicetify"
-	fperms +x "${INSTALLDIR}/spicetify-cli"
+	doins -r {CustomApps,Extensions,Themes,jsHelper,cli}
+	newbin - spicetify <<-EOF
+	#!/usr/bin/env sh
+	exec /opt/spicetify-cli/cli \$@
+	EOF
+	fperms +x "${INSTALLDIR}/cli"
 }
 
 pkg_postinst() {